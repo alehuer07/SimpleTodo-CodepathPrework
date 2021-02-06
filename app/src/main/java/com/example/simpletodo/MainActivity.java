82d4a4719ef00e55c34d76fc888dcac095deec78
Alexis Huerta <alehuer90@gmail.com>
2021-02-06 15:40:07 -0800

    setup recyclerview for todo items

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.simpletodo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
+    // Declare Variables
+
+    // Model
+    List<String> items;
+    // View Components
+    Button btnAdd;
+    EditText etItem;
+    RecyclerView rvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Variables
+
+        // Model
+        items = new ArrayList<>();
+        items.add("Buy milk");
+        items.add("Go to the gym");
+        items.add("Have fun!");
+        // View
+        btnAdd = findViewById(R.id.btnAdd);
+        etItem = findViewById(R.id.etItem);
+        rvItems = findViewById(R.id.rvItems);
+
+        //Adapter
+        ItemsAdapter itemsAdapter = new ItemsAdapter(items);
+        rvItems.setAdapter(itemsAdapter);
+        rvItems.setLayoutManager(new LinearLayoutManager(this));
+
+
+
     }
 }
